9932a4d10f9cd626999d60058640c94cd4ea41d5
rmscjf2002 <82512951+rmscjf2002@users.noreply.github.com>
2022-08-31 23:23:56 +0900

    Create MySQL.h

---
+++ MySQL.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#ifndef __MYSQL_H__
+#define __MYSQL_H__
+
+// mysql 관련 헤더파일 
+
+#include <my_global.h>
+#include <mysql.h>
+#pragma comment(lib, "libmysql.lib")
+////////////////////////////
+
+// mysql 관련 define //
+
+#define HOST "localhost"
+#define USER "root"
+#define PASS "134655"
+#define DB_NAME "library"
+#define CHOP(x) x[strlen(x) -1 ] = '\0' 
+
+///////////////////////
+
+#endif
+#pragma once
